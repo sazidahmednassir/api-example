3559d11c167bbb86df7f65922f0b657f0fb911cd
sazidahmednassir <sazidahmednassir@gmail.com>
2022-02-15 08:05:24 -0800

    nine

--- js/mealdbsync.js
+++ js/mealdbsync.js
@@ -1,32 +1,36 @@
-document.getElementById('error-message').style.display = 'none';
-const searchFood = () => {
+const searchFood = async () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     // clear data
     searchField.value = '';
-    // console.log(searchText)
-    document.getElementById('error-message').style.display = 'none';
     if (searchText == '') {
         // please write something to display
-        alert("Enter some food name");
     }
     else {
         // load data
         const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => displaySearchResult(data.meals))
-            .catch(error => displayError(error));
-    }
-}
 
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            displaySearchResult(data.meals)
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+        // fetch(url)
+        //     .then(res => res.json())
+        //     .then(data => displaySearchResult(data.meals));
+    }
+
+}
 
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     if (meals.length == 0) {
         // show no result found;
-        alert('No Result Found');
     }
     meals.forEach(meal => {
         // console.log(meal);
@@ -45,19 +49,21 @@ const displaySearchResult = meals => {
     })
 }
 
-
-const loadMealDetail = mealId => {
+const loadMealDetail = async mealId => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayMealDetail(data.meals[0]));
-}
 
+    const res = await fetch(url);
+    const data = await res.json();
+    displayMealDetail(data.meals[0]);
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => displayMealDetail(data.meals[0]));
+}
 
 const displayMealDetail = meal => {
     console.log(meal);
     const mealDetails = document.getElementById('meal-details');
-    mealDetails.textContent='';
+    mealDetails.textContent = '';
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
@@ -65,7 +71,7 @@ const displayMealDetail = meal => {
     <div class="card-body">
         <h5 class="card-title">${meal.strMeal}</h5>
         <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
-        <a href="${meal.strYoutube}" class="btn btn-primary">See No Youtube</a>
+        <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
     </div>
     `;
     mealDetails.appendChild(div);
